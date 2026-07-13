10d42398cc004b84d5d99f55c9e51b9b44468c47
Rainata Putra <kriegcom77@gmail.com>
2026-07-13 14:16:20 +0800

    Add log out option to header user menu

--- src/components/header/user-setting.tsx
+++ src/components/header/user-setting.tsx
@@ -1,28 +1,46 @@
 "use client"
-import { Settings, UserRoundCog } from "lucide-react";
+import { CircleUserRound, LogOut, UserRoundCog } from "lucide-react";
 import { Button } from "../ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { useRouter } from "next/navigation";
-import { EmployeeRoutes } from "@/constants/general";
+import { EmployeeRoutes, Routes } from "@/constants/general";
+import { useUserStore } from "@/stores/useAuthStore";
+import { useTranslation } from "react-i18next";
 
 
 const UserSetting = () => {
     const { push } = useRouter()
+    const { logout } = useUserStore()
+    const { t } = useTranslation()
+
+    const handleLogout = () => {
+        logout()
+        push(Routes.Login)
+    }
+
     return (
         <Popover>
             <PopoverTrigger asChild>
                 <Button variant={"outline"} size={"icon"} className="text-muted-foreground">
-                    <Settings/>
+                    <CircleUserRound/>
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="w-36 p-1">
-                <Button 
+                <Button
                 onClick={() => push(EmployeeRoutes.Profile("me"))}
-                variant={"ghost"} 
-                size={"sm"} 
+                variant={"ghost"}
+                size={"sm"}
                 className="w-full font-normal justify-start">
                     <UserRoundCog />
-                    Profile Settings
+                    {t("header.profileSettings")}
+                </Button>
+                <Button
+                onClick={handleLogout}
+                variant={"ghostDestructive"}
+                size={"sm"}
+                className="w-full font-normal justify-start text-destructive">
+                    <LogOut />
+                    {t("sidebar.logout")}
                 </Button>
             </PopoverContent>
         </Popover>
